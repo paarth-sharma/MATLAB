0fbfb90f0623b39b54d8a3a62ac71d7900cfaea9
paarth-sharma <paarthsharma2003@gmail.com>
2022-11-22 10:41:28 +0530

    added range-kutta method

--- range_kutta.m
+++ range_kutta.m
@@ -1,20 +1,22 @@
-clc;
+clc;                                              
 clear all;
-A = @(x)((512*pi)/x)
-f = @(x, t)((-0.6*pi*r*r)*sqrt(2*g)*sqrt(x))/(A(x));
 
-a = 0;
-b = 1;
-h = 0.2;
-n = (b-a)/h;
+dydt = @(t, y)(-0.6*pi*0.1*0.1*sqrt(2*32.1)*sqrt(t))/((512*pi)/(3*t));
+t0=input('Enter the value of t0 :  \n');
+y0=input('Enter the value of y0 :  \n');
+tn=input('Enter the value of t for which you want to find the value of y :  \n');
+h=input('Enter the step length :  \n');                                      
+i=0;
 
-t(1) = 0;
-y(1) = 1;
+while i<tn
+    k_1 = dydt(t0,y0);
+    k_2 = dydt(t0+0.5*h,y0+0.5*h*k_1);
+    k_3 = dydt((t0+0.5*h),(y0+0.5*h*k_2));
+    k_4 = dydt(((t0)+h),(y0+k_3*h));
+    next_y = y0 + ((1/6)*(k_1+2*k_2+2*k_3+k_4)*h);
+    y0 = next_y;
+    t0 = t0+h;
+    i=i+h;
+end 
 
-for i=1:n
-    t(i+1) = t(i) + h;
-    k1 = h * (f())
-
-end
-
-disp(y);
+fprintf('The value of y at t=%f is %f',t0,y0);
